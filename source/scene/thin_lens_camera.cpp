b32563722e880e5a53924304bfe27678402ab067
Adrian Astley <adastley@gmail.com>
2016-02-19 10:44:19 -0600

    Cache cartesian camera axes
    A quick Very Sleepy profile run showed CalculateRayFromPixel()
    to be taking the same amount of time per frame as scene intersection.
    This should be very obvious, since we're doing a ton of math
    over and over again, for each pixel. The original camera
    code came from a real-time engine, where it is expected for the
    camera to move quite often. However, for path tracing, the camera
    should move very infrequently, if at all. After implementing
    the caching, scene traversal is 88% of the frame time, and
    CalculateRayFromPixel() is 0.11%

---
+++ source/scene/thin_lens_camera.cpp
@@ -0,0 +1,111 @@
+/* Lantern - A path tracer
+*
+* Lantern is the legal property of Adrian Astley
+* Copyright Adrian Astley 2015 - 2016
+*/
+
+#include "scene/thin_lens_camera.h"
+
+#include <embree2/rtcore.h>
+
+
+namespace Lantern {
+
+ThinLensCamera::ThinLensCamera()
+		: m_phi(M_PI_2),
+		  m_theta(0.0f),
+		  m_radius(10.0f),
+		  m_up(1.0f),
+		  m_target(0.0f, 0.0f, 0.0f),
+		  m_tanFovXDiv2(0.5773503f /* tan(60 degrees / 2) */),
+		  m_tanFovYDiv2(std::tanf(0.5235f /* 60 degrees / 2 */ * 720 / 1280)),
+		  FrameBuffer(1280, 720) {
+	UpdateOrigin();
+	UpdateCartesianCoordSystem();
+}
+
+ThinLensCamera::ThinLensCamera(float phi, float theta, float radius, float clientWidth, float clientHeight, float fov)
+		: m_phi(phi),
+		  m_theta(theta),
+		  m_radius(radius),
+		  m_up(1.0f),
+		  m_target(0.0f, 0.0f, 0.0f),
+		  m_tanFovXDiv2(std::tanf(fov * 0.5f)),
+		  m_tanFovYDiv2(std::tanf(fov * 0.5f * clientHeight / clientWidth)),
+		  FrameBuffer(clientWidth, clientHeight) {
+	UpdateOrigin();
+	UpdateCartesianCoordSystem();
+}
+
+void ThinLensCamera::Rotate(float dPhi, float dTheta) {
+	if (m_up > 0.0f) {
+		m_theta += dTheta;
+	} else {
+		m_theta -= dTheta;
+	}
+
+	m_phi += dPhi;
+
+	// Keep phi within -2PI to +2PI for easy 'up' comparison
+	if (m_phi > 2.0 * M_PI) {
+		m_phi -= 2.0 * M_PI;
+	} else if (m_phi < -2.0 * M_PI) {
+		m_phi += 2.0 * M_PI;
+	}
+
+	// If phi is between 0 to PI or -PI to -2PI, make 'up' be positive Y, other wise make it negative Y
+	if ((m_phi > 0 && m_phi < M_PI) || (m_phi < -M_PI && m_phi > -2.0 * M_PI)) {
+		m_up = 1.0f;
+	} else {
+		m_up = -1.0f;
+	}
+
+	UpdateOrigin();
+	UpdateCartesianCoordSystem();
+}
+
+void ThinLensCamera::Zoom(float distance) {
+	m_radius -= distance;
+
+	// Don't let the radius go negative
+	// If it does, re-project our target down the look vector
+	if (m_radius <= 0.0f) {
+		m_radius = 30.0f;
+		m_target += m_zAxis * 30.0f;
+	}
+
+	UpdateOrigin();
+}
+
+void ThinLensCamera::Pan(float dx, float dy) {
+	m_target += (m_xAxis * dx) + (m_yAxis * dy);
+
+	UpdateOrigin();
+}
+
+RTCRay ThinLensCamera::CalculateRayFromPixel(uint x, uint y) const {
+	RTCRay ray;
+
+	ray.org = m_origin;
+
+	ray.tnear = 0.0f;
+	ray.tfar = embree::inf;
+	ray.geomID = RTC_INVALID_GEOMETRY_ID;
+	ray.primID = RTC_INVALID_GEOMETRY_ID;
+	ray.instID = RTC_INVALID_GEOMETRY_ID;
+	ray.mask = 0xFFFFFFFF;
+	ray.time = 0.0f;
+
+	float3a viewVector((((x + 0.5f) / FrameBuffer.Width) * 2.0f - 1.0f) * m_tanFovXDiv2,
+	                   -(((y + 0.5f) / FrameBuffer.Height) * 2.0f - 1.0f) * m_tanFovYDiv2,
+	                   -1.0f);
+
+	// Matrix multiply
+	ray.dir = normalize(float3a(dot(viewVector, m_matrixMulXAxis),
+	                            dot(viewVector, m_matrixMulYAxis),
+	                            dot(viewVector, m_matrixMulZAxis)));
+
+	return ray;
+}
+
+} // End of namespace Lantern
---
+++ source/scene/thin_lens_camera.h
@@ -0,0 +1,121 @@
+/* Lantern - A path tracer
+*
+* Lantern is the legal property of Adrian Astley
+* Copyright Adrian Astley 2015 - 2016
+*/
+
+#pragma once
+
+#include "common/typedefs.h"
+
+#include "math/vector_types.h"
+
+#include "scene/ray.h"
+
+#include "graphics/frame_buffer.h"
+
+
+namespace Lantern {
+
+class ThinLensCamera {
+public:
+	ThinLensCamera();
+	ThinLensCamera(float phi, float theta, float radius, float clientWidth, float clientHeight, float fov = M_PI_2);
+
+public:
+	// Frame Data
+	FrameBuffer FrameBuffer;
+
+private:
+	// Frame Data
+
+
+	// Spherical data
+	float m_phi;
+	float m_theta;
+	float m_radius;
+
+	float m_up;
+
+	float3 m_target;
+
+	// Cartesian Data
+	float3 m_origin;
+
+	float3 m_xAxis;
+	float3 m_yAxis;
+	float3 m_zAxis;
+
+	// Ray Transform Data
+	float m_tanFovXDiv2;
+	float m_tanFovYDiv2;
+
+	float3a m_matrixMulXAxis;
+	float3a m_matrixMulYAxis;
+	float3a m_matrixMulZAxis;
+
+public:
+	/**
+	 * Rotate the camera about a point in front of it (m_target). 
+	 * 
+	 * Phi tilts the camera up and down. 
+	 * Theta rotates the camera side to side. 
+	 *
+	 * @param dPhi      The number of radians to rotate in the phi direction
+	 * @param dTheta    The number of radians to rotate in the theta direction
+	 */
+	void Rotate(float dPhi, float dTheta);
+	/**
+	 * Move the camera down the look vector, closer to m_target. If we overtake m_target,
+	 * it is reprojected 30 units down the look vector
+	 *
+	 * TODO: Find a way to *not* hard-code the reprojection distance. Perhaps base it on the
+	 *       scene size? 
+	 *
+	 * @param distance    The distance to zoom. Negative distance will move the camera away from the target, positive will move towards
+	 */
+	void Zoom(float distance);
+	/**
+	 * Moves the camera within its local X-Y plane
+	 *
+	 * @param dx    The amount to move the camera right or left
+	 * @param dy    The amount to move the camera up or down
+	 */
+	void Pan(float dx, float dy);
+
+	/**
+	 * Calculates the world-space ray from the camera origin to the specified pixel
+	 *
+	 * @param x         The x coordinate of the pixel
+	 * @param y         The y coordinate of the pixel
+	 */
+	RTCRay CalculateRayFromPixel(uint x, uint y) const;
+
+private:
+	/**
+	* Returns the position of the camera in Cartesian coordinates
+	*
+	* @return    The position of the camera
+	*/
+	void UpdateOrigin() {
+		float x = m_radius * std::sinf(m_phi) * std::sinf(m_theta);
+		float y = m_radius * std::cosf(m_phi);
+		float z = m_radius * std::sinf(m_phi) * std::cosf(m_theta);
+
+		m_origin = m_target + float3(x, y, z);
+	}
+
+	void UpdateCartesianCoordSystem() {
+		float3 worldUp(0.0f, m_up, 0.0f);
+
+		m_zAxis = normalize(m_origin - m_target);
+		m_xAxis = normalize(cross(worldUp, m_zAxis));
+		m_yAxis = cross(m_zAxis, m_xAxis);
+
+		m_matrixMulXAxis = float3a(m_xAxis.x, m_yAxis.x, m_zAxis.x);
+		m_matrixMulYAxis = float3a(m_xAxis.y, m_yAxis.y, m_zAxis.y);
+		m_matrixMulZAxis = float3a(m_xAxis.z, m_yAxis.z, m_zAxis.z);
+	}
+};
+
+} // End of namespace Lantern
