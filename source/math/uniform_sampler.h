c7e8e4f8c3b4568bab26a3cb4e7f316c6929067f
Adrian Astley <adastley@gmail.com>
2016-02-26 00:21:15 -0600

    Correct the psuedo-random number generation
    1. If we seed with a int32, the first UInt will always be 0,
    due to how the upper bits get shifted. Therefore, to get a
    legitimate value, we need to call GetUInt() once before we actually
    use it.
    
    2. Use a more simplified algorithm to get a float from a uint range.

--- source/math/uniform_sampler.h
+++ source/math/uniform_sampler.h
@@ -17,6 +17,7 @@ public:
 	UniformSampler(uint64 seed, uint64 sequence = 0)
 		: m_state(seed),
 		  m_sequence(sequence) {
+		NextUInt();
 	}
 
 private:
@@ -33,15 +34,12 @@ public:
 		uint32 xorShifted = ((oldState >> 18u) ^ oldState) >> 27u;
 		uint32 rot = oldState >> 59u;
 
-		return (xorShifted >> rot) | (xorShifted << (uint32(-int32(rot)) & 31));
+		return (xorShifted >> rot) | (xorShifted << ((-rot) & 31));
 	}
 
-	// Implementation of this paper: http://www.doornik.com/research/randomdouble.pdf
-	#define INVERSE_MAX_UINT32 2.32830643653869628906e-010 /* 1 / (2^-32) */
-	#define UINT_TO_UNIFORM_FLOAT(input) ((int)(input) * INVERSE_MAX_UINT32 + (0.5 + INVERSE_MAX_UINT32 / 2))
-
 	float NextFloat() {
-		return UINT_TO_UNIFORM_FLOAT(NextUInt());
+		uint32 temp = NextUInt();
+		return std::ldexp(temp, -32);
 	}
 
 	float2 NextFloat2() {
