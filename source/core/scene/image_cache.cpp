0c615c5731f90d248e660ced4d404415721f6ea6
Adrian Astley <adastley@gmail.com>
2017-10-06 20:17:36 -0700

    Swap row and column
    I can't math / graph properly... Row and column were swapped. To prevent
    further issues, I just switched to using X and Y

--- source/core/scene/image_cache.cpp
+++ source/core/scene/image_cache.cpp
@@ -24,15 +24,15 @@ float3 ImageCache::SampleImage(uint imageId, float2 texCoord) {
 	Image *image = &m_images[imageId];
 
 	// Closest sampling for now because it's simple
-	std::size_t row = (uint)std::round(texCoord.x * (float)image->XSize);
-	std::size_t column = (uint)std::round(texCoord.y * (float)image->YSize);
+	std::size_t x = (uint)std::round(texCoord.x * (float)image->XSize);
+	std::size_t y = (uint)std::round(texCoord.y * (float)image->YSize);
 
-	std::size_t index = (row * image->XSize + column) * image->NumChannels;
+	std::size_t index = (y * image->XSize + x) * image->NumChannels;
 	switch (image->NumChannels) {
 	case 1:
 	{
 		float value = (float)image->Data[index] / 256.0f;
-		return float3(value, value, value);
+		return float3(value);
 	}
 	case 2:
 		return float3((float)image->Data[index] / 256.0f, (float)image->Data[index + 1] / 256.0f, 0.0f);
