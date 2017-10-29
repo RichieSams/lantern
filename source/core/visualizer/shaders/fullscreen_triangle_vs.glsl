f1c82f9c12348ec3bed7ea6bb911f4dae93b78fe
Adrian Astley <adastley@gmail.com>
2017-10-28 22:46:28 -0700

    Implement a fullscreen triangle shader

--- source/core/visualizer/shaders/fullscreen_triangle_vs.glsl
+++ source/core/visualizer/shaders/fullscreen_triangle_vs.glsl
@@ -1,25 +1,13 @@
 #version 450
-#extension GL_ARB_separate_shader_objects : enable
 
 out gl_PerVertex {
     vec4 gl_Position;
 };
 
-layout(location = 0) out vec3 fragColor;
+layout(location = 0) out vec2 texCoord;
 
-vec2 positions[3] = vec2[](
-    vec2(0.0, -0.5),
-    vec2(0.5, 0.5),
-    vec2(-0.5, 0.5)
-);
-
-vec3 colors[3] = vec3[](
-    vec3(1.0, 0.0, 0.0),
-    vec3(0.0, 1.0, 0.0),
-    vec3(0.0, 0.0, 1.0)
-);
 
 void main() {
-    gl_Position = vec4(positions[gl_VertexIndex], 0.0, 1.0);
-    fragColor = colors[gl_VertexIndex];
+	texCoord = vec2((gl_VertexIndex << 1) & 2, gl_VertexIndex & 2);
+	gl_Position = vec4(texCoord * 2.0f + -1.0f, 0.0f, 1.0f);
 }
