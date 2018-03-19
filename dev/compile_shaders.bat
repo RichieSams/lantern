c48454d08781ccfa4a81db45ea006b0997bde205
Adrian Astley <adastley@gmail.com>
2018-03-19 09:02:57 -0700

    Create a non-portable bat file for compiling the shader files

---
+++ dev/compile_shaders.bat
@@ -0,0 +1,2 @@
+T:/Applications/VulkanSDK/1.0.61.1/Bin/glslangValidator.exe -V -o ../source/core/visualizer/shaders/fullscreen_triangle_vs.spv -S vert ../source/core/visualizer/shaders/fullscreen_triangle_vs.glsl
+T:/Applications/VulkanSDK/1.0.61.1/Bin/glslangValidator.exe -V -o ../source/core/visualizer/shaders/final_resolve_ps.spv -S frag ../source/core/visualizer/shaders/final_resolve_ps.glsl
